3494fd225e43cba333ebc42957b74e695827c65a
Tom <thomasgrosjean00@gmail.com>
2024-08-24 08:17:00 +0200

    build

--- dist/mapbox-gl-export.es.js
+++
@@ -1,9 +0,0 @@
-import { u as t, C as r, M as s, t as e, _ as i } from "./index-yQeS8ugB.mjs";
-export {
-  t as DPI,
-  r as Format,
-  s as MapboxExportControl,
-  e as PageOrientation,
-  i as Size
-};
-//# sourceMappingURL=mapbox-gl-export.es.js.map
--- dist/types/map-generator.d.ts
+++ dist/types/map-generator.d.ts
@@ -1,9 +1,31 @@
-import mapboxgl, { Map as MapboxMap } from 'mapbox-gl';
+import { Map as MapboxMap } from 'mapbox-gl';
 import 'js-loading-overlay';
 import { DPIType, FormatType, MapGeneratorBase, SizeType, UnitType } from '@watergis/maplibre-gl-export';
 export default class MapGenerator extends MapGeneratorBase {
     private accesstoken;
-    constructor(map: MapboxMap, size?: SizeType, dpi?: DPIType, format?: FormatType, unit?: UnitType, fileName?: string, markerCirclePaint?: mapboxgl.CirclePaint, attributionOptions?: import("@watergis/maplibre-gl-export").AttributionOptions, northIconOptions?: import("@watergis/maplibre-gl-export").NorthIconOptions, accesstoken?: string);
-    protected getRenderedMap(container: HTMLElement, style: mapboxgl.Style): mapboxgl.Map;
+    constructor(map: MapboxMap, size?: SizeType, dpi?: DPIType, format?: FormatType, unit?: UnitType, fileName?: string, markerCirclePaint?: {
+        "circle-radius"?: import("mapbox-gl").DataDrivenPropertyValueSpecification<number>;
+        "circle-radius-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-color"?: import("mapbox-gl").DataDrivenPropertyValueSpecification<import("mapbox-gl").ColorSpecification>;
+        "circle-color-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-blur"?: import("mapbox-gl").DataDrivenPropertyValueSpecification<number>;
+        "circle-blur-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-opacity"?: import("mapbox-gl").DataDrivenPropertyValueSpecification<number>;
+        "circle-opacity-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-translate"?: import("mapbox-gl").PropertyValueSpecification<[number, number]>;
+        "circle-translate-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-translate-anchor"?: import("mapbox-gl").PropertyValueSpecification<"map" | "viewport">;
+        "circle-pitch-scale"?: import("mapbox-gl").PropertyValueSpecification<"map" | "viewport">;
+        "circle-pitch-alignment"?: import("mapbox-gl").PropertyValueSpecification<"map" | "viewport">;
+        "circle-stroke-width"?: import("mapbox-gl").DataDrivenPropertyValueSpecification<number>;
+        "circle-stroke-width-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-stroke-color"?: import("mapbox-gl").DataDrivenPropertyValueSpecification<import("mapbox-gl").ColorSpecification>;
+        "circle-stroke-color-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-stroke-opacity"?: import("mapbox-gl").DataDrivenPropertyValueSpecification<number>;
+        "circle-stroke-opacity-transition"?: import("mapbox-gl").TransitionSpecification;
+        "circle-emissive-strength"?: import("mapbox-gl").PropertyValueSpecification<number>;
+        "circle-emissive-strength-transition"?: import("mapbox-gl").TransitionSpecification;
+    } | undefined, attributionOptions?: import("@watergis/maplibre-gl-export").AttributionOptions, northIconOptions?: import("@watergis/maplibre-gl-export").NorthIconOptions, accesstoken?: string);
+    protected getRenderedMap(container: HTMLElement, style: mapboxgl.Style): MapboxMap;
 }
 //# sourceMappingURL=map-generator.d.ts.map
